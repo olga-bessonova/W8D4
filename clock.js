77ecb3ac56b2d18a33676971c6cb94991983077b
Olga <olga.sleepless@gmail.com>
2023-03-02 16:19:31 -0500

    Update _tick

--- clock.js
+++ clock.js
@@ -19,15 +19,38 @@ function Clock(){
   this.minutes = date.getMinutes();
   this.hour = date.getHours();
   // this.time = `${this.hour}:${this.minutes}:${this.seconds}`
-  
+  this.printTime();
+  let bindTick = this._tick.bind(this);
+  window.setInterval(
+    // call your function here
+    bindTick, 1000);
 };
 
 
 Clock.prototype.printTime = function() {
-  return (`${this.hour}:${this.minutes}:${this.seconds}`);
+  //return (`${this.hour}:${this.minutes}:${this.seconds}`);
+  // console.log(`${this.hour % 24}:${this.minutes % 60}:${this.seconds % 60}`);
+  console.log(`${this.hour}:${this.minutes}:${this.seconds}`);
+  
 };
 
 Clock.prototype._tick = function () {
+  // debugger
   this.seconds += 1;
-  return this.printTime();
-}
+  if (this.seconds === 60) {
+    this.minutes++;
+    this.seconds = 0;    
+  };
+  if (this.minutes === 60) {
+    this.hour++;
+    this.minutes = 0;
+  };
+  if (this.hour === 24) {
+    this.hour = 0;
+  };
+  //return this.printTime();
+  this.printTime();
+
+};
+
+const clock = new Clock();
